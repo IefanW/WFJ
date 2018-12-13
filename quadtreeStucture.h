bf0313e7c4a686d7a17cf11a0263b4e62f9fb902
IefanW <45580928+IefanW@users.noreply.github.com>
2018-12-13 23:20:02 +0800

    Create quadtreeStucture.h

---
+++ quadtreeStucture.h
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "structureTree.h"
+
+#ifndef QUADTREESTRUCTURE_H
+#define QUADTREESTRUCTURE_H
+double nodeValue(Node * node,double time);
+double value (double x,double y,double time) ;
+#endif
